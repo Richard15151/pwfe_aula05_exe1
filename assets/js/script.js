1acc29639fdbac2345261894dd7e39277b33e3ef
Richard15151 <richard.oliveira.senai@gmail.com>
2025-03-07 10:06:23 -0300

    criação das funções js para funcionamento do menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
 function mudaFoto(item){
-
+    if (item == 1){
+        document.getElementById("icone").src="assets/img/home.png";
+    }else if (item == 2){
+        document.getElementById("icone").src="assets/img/cardapio.png";
+    }else if (item == 3){
+        document.getElementById("icone").src="assets/img/sobre.png";
+    }else if (item == 4){
+        document.getElementById("icone").src="assets/img/contato.png";
+    }else if (item == 5){
+        document.getElementById("icone").src="assets/img/logo.png";
+    }
 }
