1513dcf72574b87e3a315074e073d58ab7d9fe88
Richard15151 <richard.oliveira.senai@gmail.com>
2025-03-07 10:44:17 -0300

    melhorias no código

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,19 @@
 function mudaFoto(item){
+    let logo = document.getElementById('icone')
     if (item == 1){
-        document.getElementById("icone").src="assets/img/home.png";
+        logo.src="assets/img/home.png";
         console.log("Logo mudou para home!")
     }else if (item == 2){
-        document.getElementById("icone").src="assets/img/cardapio.png";
+        logo.src="assets/img/cardapio.png";
         console.log("Logo mudou para cardápio!")
     }else if (item == 3){
-        document.getElementById("icone").src="assets/img/sobre.png";
+        logo.src="assets/img/sobre.png";
         console.log("Logo mudou para sobre!")
     }else if (item == 4){
-        document.getElementById("icone").src="assets/img/contato.png";
+        logo.src="assets/img/contato.png";
         console.log("Logo mudou para contato!")
     }else if (item == 5){
-        document.getElementById("icone").src="assets/img/logo.png";
+        logo.src="assets/img/logo.png";
         console.log("Logo voltou para a logo normal!")
     }
 }
