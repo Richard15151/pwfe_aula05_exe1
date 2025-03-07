7154b8b1f96f002701a95627e8a888c1a4724605
Richard15151 <richard.oliveira.senai@gmail.com>
2025-03-07 10:31:39 -0300

    usando console.log para melhor entendimento do funcionamento

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,18 @@
 function mudaFoto(item){
     if (item == 1){
         document.getElementById("icone").src="assets/img/home.png";
+        console.log("Logo mudou para home!")
     }else if (item == 2){
         document.getElementById("icone").src="assets/img/cardapio.png";
+        console.log("Logo mudou para cardápio!")
     }else if (item == 3){
         document.getElementById("icone").src="assets/img/sobre.png";
+        console.log("Logo mudou para sobre!")
     }else if (item == 4){
         document.getElementById("icone").src="assets/img/contato.png";
+        console.log("Logo mudou para contato!")
     }else if (item == 5){
         document.getElementById("icone").src="assets/img/logo.png";
+        console.log("Logo voltou para a logo normal!")
     }
 }
