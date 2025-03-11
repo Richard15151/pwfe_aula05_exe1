b9ee4bfb3a3d37a0a72fc7f3763cfeaac932c5a0
Richard15151 <richard.oliveira.senai@gmail.com>
2025-03-11 10:17:12 -0300

    adcionando js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,22 @@
 function mudaFoto(item){
     let logo = document.getElementById('icone')
     if (item == 1){
-        logo.src="assets/img/home.png";
-        console.log("Logo mudou para home!")
+        logo.src="assets/img/computadores.png";
+        console.log("Logo mudou para computadores!")
     }else if (item == 2){
-        logo.src="assets/img/cardapio.png";
-        console.log("Logo mudou para cardápio!")
+        logo.src="assets/img/videogames.png";
+        console.log("Logo mudou para vídeo games!")
     }else if (item == 3){
-        logo.src="assets/img/sobre.png";
-        console.log("Logo mudou para sobre!")
+        logo.src="assets/img/celulares.png";
+        console.log("Logo mudou para celulares!")
     }else if (item == 4){
         logo.src="assets/img/contato.png";
         console.log("Logo mudou para contato!")
     }else if (item == 5){
-        logo.src="assets/img/logo.png";
+        logo.src="assets/img/quemsomos.png";
+        console.log("Logo mudou para quem somos!")
+    }else if (item == 6){
+        logo.src="assets/img/principal.png";
         console.log("Logo voltou para a logo normal!")
     }
 }
